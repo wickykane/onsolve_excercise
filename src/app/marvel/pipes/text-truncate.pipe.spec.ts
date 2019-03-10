03c1c60319dc31bb0ebdfd244962de4308889f5a
Quang Huy <wickykane@gmail.com>
2019-03-10 20:36:37 +0700

    update unit test

--- src/app/marvel/pipes/text-truncate.pipe.spec.ts
+++ src/app/marvel/pipes/text-truncate.pipe.spec.ts
@@ -6,9 +6,19 @@ describe('TextTruncatePipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('should equal "Test ..."', () => {
+  it('should equal text when text length less than limit', () => {
     const pipe = new TextTruncatePipe();
     expect(pipe.truncateText('Test', 8)).toEqual('Test');
+  });
+
+  it('should truncate text', () => {
+    const pipe = new TextTruncatePipe();
+    expect(pipe.truncateText('This is a test text', 8)).toEqual('This is ...');
+  });
+
+  it('should equal undefined', () => {
+    const pipe = new TextTruncatePipe();
+    expect(pipe.truncateText(undefined, 8)).toBeUndefined();
   });
 
 });
