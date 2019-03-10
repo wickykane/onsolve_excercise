22daed513b109fa8050fc522dd93f54492285259
Quang Huy <wickykane@gmail.com>
2019-03-10 20:25:08 +0700

    update

--- src/app/exercise/pipes/remove-vowel.pipe.spec.ts
+++ src/app/exercise/pipes/remove-vowel.pipe.spec.ts
@@ -6,11 +6,65 @@ describe('RemoveVowelPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
+  it('should remove e', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('e')).toEqual('');
+  });
+
+  it('should remove o', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('o')).toEqual('');
+  });
+
+  it('should remove u', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('u')).toEqual('');
+  });
+
+  it('should remove a', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('a')).toEqual('');
+  });
+
+  it('should remove i', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('i')).toEqual('');
+  });
+
+  it('should remove E', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('E')).toEqual('');
+  });
+
+  it('should remove O', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('O')).toEqual('');
+  });
+
+  it('should remove U', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('U')).toEqual('');
+  });
+
+  it('should remove A', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('A')).toEqual('');
+  });
+
+  it('should remove I', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('I')).toEqual('');
+  });
+
   it('should equal "Tst"', () => {
     const pipe = new RemoveVowelPipe();
     expect(pipe.removeVowel('Test')).toEqual('Tst');
   });
 
+  it('should equal "Tst"', () => {
+    const pipe = new RemoveVowelPipe();
+    expect(pipe.removeVowel('Test')).toEqual('Tst');
+  });
   it('should equal "Ths wbst s fr lsrs LL!"', () => {
     const pipe = new RemoveVowelPipe();
     const text = 'This website is for losers LOL!';
